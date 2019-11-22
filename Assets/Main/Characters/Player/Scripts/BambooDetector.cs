0895d21c1a6dd27c5af46cc7d75e67a1b75a954a
PridesFall08 <adi.pop.bv@gmail.com>
2019-11-22 17:03:01 +0200

    Added bamboo grabbing

---
+++ Assets/Main/Characters/Player/Scripts/BambooDetector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BambooDetector : MonoBehaviour
+{
+    public Transform mouth;
+
+    void OnTriggerStay(Collider other)
+    {
+        print(other.gameObject.tag);
+        if (Input.GetButtonDown("Jump") && other.gameObject.CompareTag("Bamboo"))
+        {
+            
+            other.transform.SetParent(mouth.transform);
+            other.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
+            other.transform.position = mouth.transform.position;
+        }
+    }
+}
