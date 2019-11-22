ff5a8e85b9838d3a91bbfb349b22e68513a8db07
PridesFall08 <adi.pop.bv@gmail.com>
2019-11-22 14:48:38 +0200

    Added player camera follow

---
+++ Assets/Main/Characters/Player/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
