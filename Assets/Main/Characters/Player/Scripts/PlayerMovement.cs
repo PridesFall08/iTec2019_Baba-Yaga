08e0007a105d530bffda6793f74c8315973357ea
PridesFall08 <adi.pop.bv@gmail.com>
2019-11-23 00:53:37 +0200

    Fixed movement and camera position

--- Assets/Main/Characters/Player/Scripts/PlayerMovement.cs
+++ Assets/Main/Characters/Player/Scripts/PlayerMovement.cs
@@ -6,6 +6,6 @@ public class PlayerMovement : MonoBehaviour
     
     void FixedUpdate()
     {
-        transform.Translate(new Vector3(Input.GetAxis("Vertical") * speed * Time.deltaTime, 0, Input.GetAxis("Horizontal") * speed * Time.deltaTime * -1));
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0,  Input.GetAxis("Vertical") * speed * Time.deltaTime));
     }
 }
