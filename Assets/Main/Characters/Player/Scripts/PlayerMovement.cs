8d1238a22100879d99a9174a85b6bb98af12a504
PridesFall08 <adi.pop.bv@gmail.com>
2019-11-23 04:21:24 +0200

    Added panda animations to player

--- Assets/Main/Characters/Player/Scripts/PlayerMovement.cs
+++ Assets/Main/Characters/Player/Scripts/PlayerMovement.cs
@@ -2,10 +2,22 @@
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Animator animator;
     public float speed;
-    
+
+    private float horizontal = 0, vertical = 0;
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
     void FixedUpdate()
     {
-        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0,  Input.GetAxis("Vertical") * speed * Time.deltaTime));
+        
+        transform.Translate(new Vector3(horizontal * speed * Time.deltaTime, 0,  vertical * speed * Time.deltaTime));
+        animator.SetFloat("Horizontal", horizontal);
+        animator.SetFloat("Vertical", vertical);
     }
 }
